a0ae7b7268875c33353f0f045baf4ce2d764c4c8
app-01 <yupeiliang110@163.com>
2017-02-04 10:55:17 +0800

    添加工程文件

---
+++ CoreAnimationTechniques/CoreAnimationTechniques/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimationTechniques
+//
+//  Created by app-01 on 2017/2/4.
+//  Copyright © 2017年 EBOOK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
