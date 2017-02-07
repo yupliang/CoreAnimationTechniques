3fbbd6b52859311bfd045250536c55510504d615
app-01 <yupeiliang110@163.com>
2017-02-07 17:47:14 +0800

    two cube with CATransformLayer

---
+++ CoreAnimationTechniques/CoreAnimationTechniques/CATELayerLabel.h
@@ -0,0 +1,13 @@
+//
+//  CATELayerLabel.h
+//  CoreAnimationTechniques
+//
+//  Created by app-01 on 2017/2/7.
+//  Copyright © 2017年 EBOOK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CATELayerLabel : UILabel
+
+@end
--- CoreAnimationTechniques/CoreAnimationTechniques/ViewController.m
+++ CoreAnimationTechniques/CoreAnimationTechniques/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import <GLKit/GLKit.h>
+#import <CoreText/CoreText.h>
+#import "CATELayerLabel.h"
 #define LIGHT_DIRECTION 0,1,-0.5
 #define AMBIENT_LIGHT 0.5
 @interface ViewController () <CALayerDelegate>
@@ -43,62 +45,75 @@ UIButton * (^getAButton)() = ^UIButton *(){
     return button;
 };
 @implementation ViewController
+- (CALayer *)faceWithTransform:(CATransform3D)transform {
+    //create cube face layer
+    CALayer *face = [CALayer layer];
+    face.frame = CGRectMake(-50, -50, 100, 100);
+    
+    //apply a random color
+    CGFloat red = rand()/(double)INT_MAX;
+    CGFloat green = rand()/(double)INT_MAX;
+    CGFloat blue = rand()/(double)INT_MAX;
+    
+    face.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1].CGColor;
+    //apply the transform and return
+    face.transform = transform;
+    return face;
+}
+- (CALayer *)cubeWithTransform:(CATransform3D)transform {
+    //create cube layer
+    CATransformLayer *cube = [CATransformLayer layer];
+    //add cube face 1
+    [cube addSublayer:[self faceWithTransform:CATransform3DMakeTranslation(0, 0, 50)]];
+    //add cube face 2
+    CATransform3D ct = CATransform3DMakeTranslation(50, 0, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    //add cube face 3
+    ct = CATransform3DMakeTranslation(0, -50, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    //add cube face 4
+    ct = CATransform3DMakeTranslation(0, 50, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    //add cube face 5
+    ct = CATransform3DMakeTranslation(-50, 0, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    //add cube face 6
+    ct = CATransform3DMakeTranslation(0, 0, -50);
+    ct = CATransform3DRotate(ct, M_PI, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //center the cube layer within the container
+    CGSize containerSize = _containerView.bounds.size;
+    cube.position = CGPointMake(containerSize.width/2.0f, containerSize.height/2.0f);
+    //apply the transform and return
+    cube.transform = transform;
+    return cube;
+}
 - (void)loadView {
     [super loadView];
-
-    UIImage *image = [UIImage imageNamed:@"IMG_k1"];
-    _containerView.layer.contents = (__bridge id)image.CGImage;
+    //set up the perspective transform
+    CATransform3D pt = CATransform3DIdentity;
+    pt.m34 = -1.0f/500.0f;
+    _containerView.layer.sublayerTransform = pt;
+    //set up the transform for cube 1 and add it
+    CATransform3D c1t = CATransform3DIdentity;
+    c1t = CATransform3DTranslate(c1t, -100, 0, 0);
+    CALayer *cube1 = [self cubeWithTransform:c1t];
+    [_containerView.layer addSublayer:cube1];
     
-    //define path parameters
-    CGRect rect = CGRectMake(50, 50, 100, 100);
-    CGSize radii = CGSizeMake(20, 20);
-    UIRectCorner corners = UIRectCornerTopRight | UIRectCornerBottomRight | UIRectCornerBottomLeft;
-    //create path
-    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:rect  byRoundingCorners:corners cornerRadii:radii];
-    
-    //create shape layer
-    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
-    shapeLayer.strokeColor = [UIColor redColor].CGColor;
-    shapeLayer.fillColor = [UIColor redColor].CGColor;
-    shapeLayer.lineWidth = 5;
-    shapeLayer.lineJoin = kCALineJoinRound;
-    shapeLayer.lineCap = kCALineCapRound;
-    shapeLayer.path = path.CGPath;
-    //add it to our view
-    _containerView.layer.mask = shapeLayer;
+    //set up the transform for cube 2 and add it
+    CATransform3D c2t = CATransform3DIdentity;
+    c2t = CATransform3DTranslate(c2t, 100, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 1, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 0, 1, 0);
+    CALayer *cube2 = [self cubeWithTransform:c2t];
+    [_containerView.layer addSublayer:cube2];
 }
 
-#pragma mark -- Matchstick Men
-- (void)matchstickMen {
-    //create path
-    UIBezierPath *path = [[UIBezierPath alloc] init];
-    [path moveToPoint:CGPointMake(175, 100)];
-    [path addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
-    [path moveToPoint:CGPointMake(140, 90)];
-    [path addLineToPoint:CGPointMake(141, 90)];
-    [path moveToPoint:CGPointMake(160, 90)];
-    [path addLineToPoint:CGPointMake(161, 90)];
-    [path moveToPoint:CGPointMake(145, 110)];
-    [path addLineToPoint:CGPointMake(155, 110)];
-    [path moveToPoint:CGPointMake(150, 125)];
-    [path addLineToPoint:CGPointMake(150, 175)];
-    [path addLineToPoint:CGPointMake(125, 225)];
-    [path moveToPoint:CGPointMake(150, 175)];
-    [path addLineToPoint:CGPointMake(175, 225)];
-    [path moveToPoint:CGPointMake(100, 150)];
-    [path addLineToPoint:CGPointMake(200, 150)];
-    
-    //create shape layer
-    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
-    shapeLayer.strokeColor = [UIColor redColor].CGColor;
-    shapeLayer.fillColor = [UIColor clearColor].CGColor;
-    shapeLayer.lineWidth = 5;
-    shapeLayer.lineJoin = kCALineJoinRound;
-    shapeLayer.lineCap = kCALineCapRound;
-    shapeLayer.path = path.CGPath;
-    //add it to our view
-    [_containerView.layer addSublayer:shapeLayer];
-}
 
 - (IBAction)tap:(id)sender {
     NSLog(@"%s",__FUNCTION__);
